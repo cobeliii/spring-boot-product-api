fd374878de154bb8c7aac0febd96a6f4385e2ae2
cobeliii <cobeliiimuname@gmail.com>
2025-07-13 11:11:28 -0500

    First commit of the project

---
+++ src/test/java/com/cobeliii/springbootproductapi/SpringBootProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobeliii.springbootproductapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
